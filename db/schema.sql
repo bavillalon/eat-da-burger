a4a27aaffd068bfffc32a199fae1928670874a06
Bryan Villalon <bavillalon@gmail.com>
2019-09-23 11:27:11 -0500

    initial files and some css to make whataburger stripes and also setup the database with 3 burgers

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS burger_db;
+
+CREATE DATABASE burger_db;
+
+USE burger_db;
+
+CREATE TABLE burgers (
+	id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(80) NULL,
+    devoured BOOLEAN NOT NULL,
+    PRIMARY KEY (id)
+    );
+    
+
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burger_db.burgers (burger_name, devoured)
+	VALUES ('Cheeseburger', FALSE),
+		('Bacon Cheeseburger with jalapeno', FALSE),
+        ('Bleu cheese burger with carmelized onion, roasted tomato, and garlic aoli', FALSE);
